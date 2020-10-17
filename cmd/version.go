11ea29d6a9e65efcfc1dc8e6f3bea8d26442d61e
Neal Lathia <neal.lathia@gmail.com>
2020-10-17 22:13:35 +0100

    Create operator version command

---
+++ cmd/version.go
@@ -0,0 +1,26 @@
+/*
+Copyright © 2020 Neal Lathia <neal.lathia@gmail.com>
+
+*/
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/operatorai/operator/config"
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Installed version of operator",
+	Long:  `Prints and installed version of the operator CLI tool.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println(config.Version)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
