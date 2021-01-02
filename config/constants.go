3ea4c271bf722467a6b3ad4930de99a1cd1a4eba
Neal Lathia <neal.lathia@gmail.com>
2021-01-02 13:39:54 +0000

    Add Lambda constants

--- config/constants.go
+++ config/constants.go
@@ -5,24 +5,27 @@ import (
 )
 
 const (
-	Version = "v0.0.1-alpha"
-
 	// viper config keys
 	CloudProvider  = "cloud_provider"
 	DeploymentType = "deployment_type"
-	Runtime        = "runtime"
+	Runtime        = "language"
 
 	// Google Cloud deployments
 	GoogleCloud      = "gcloud"
 	ProjectID        = "project_id"
 	DeploymentRegion = "region"
 
+	// AWS deployments
+	// @TODO
+
 	// Deployment types
 	GoogleCloudFunction = "functions"
 	GoogleCloudRun      = "run"
+	AWSLambda           = "lambda"
 
-	// Supported languages (just Python right now)
+	// Supported languages
 	Python = "python37"
+	GoLang = "go113"
 
 	// Service config file name
 	DeploymentConfig = "operator.config"
@@ -31,17 +34,21 @@ const (
 var DeploymentTypes = mapset.NewSetWith(
 	GoogleCloudFunction,
 	GoogleCloudRun,
+	AWSLambda,
 )
 
 var DeploymentNames = map[string]string{
 	"Google Cloud Function": GoogleCloudFunction,
 	"Google Cloud Run":      GoogleCloudRun,
+	"AWS Lambda":            AWSLambda,
 }
 
 var Runtimes = mapset.NewSetWith(
 	Python,
+	GoLang,
 )
 
 var RuntimeNames = map[string]string{
 	"Python (3.7)": Python,
+	"Go (1.13)":    GoLang,
 }
