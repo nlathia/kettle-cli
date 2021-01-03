4f0d5e4dafe169896168a1c82b0519256f31fe70
Neal Lathia <neal.lathia@gmail.com>
2021-01-03 17:17:50 +0000

    Fix path in templates

--- clouds/aws_lambda.go
+++ clouds/aws_lambda.go
@@ -5,9 +5,26 @@ import "github.com/operatorai/operator/config"
 type AWSLambdaFunction struct{}
 
 func (AWSLambdaFunction) Setup() error {
+	// @TODO: enable selecting whether to create .zip or image-based lambdas
 	return nil
 }
 
 func (AWSLambdaFunction) Deploy(directory string, config *config.TemplateConfig) error {
+	/*
+		1. Build the docker image
+		2. Push the image to ECR
+		3. Deploy the function
+	*/
+
+	/*
+		Example:
+		$ aws lambda create-function \
+			--function-name my-function \
+			--package-type Image \
+			--code # URI of a container image in the Amazon ECR registry.
+			--handler index.handler \
+			--runtime nodejs12.x \x
+			--role arn:aws:iam::123456789012:role/lambda-ex
+	*/
 	return nil
 }
