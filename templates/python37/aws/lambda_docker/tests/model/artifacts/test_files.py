aa89ee54e7f8a1d050a844b8ee550d0598e96a12
Neal Lathia <neal.lathia@gmail.com>
2021-01-03 16:24:58 +0000

    Create lambda_docker

---
+++ templates/python37/aws/lambda_docker/tests/model/artifacts/test_files.py
@@ -0,0 +1,10 @@
+import os
+
+from model.artifacts import files
+
+
+def test_get_path():
+    # get_path() returns the path of a file in the artifacts directory
+    # we test this by loading the path to the .py file itself
+    path = files.get_path("files.py")
+    assert os.path.exists(path)
