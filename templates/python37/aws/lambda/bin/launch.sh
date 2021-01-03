cf89a08ca0c14a07dc4b0e7c9378367656d9e41b
Neal Lathia <neal.lathia@gmail.com>
2021-01-03 12:39:13 +0000

    Add missing space

--- templates/python37/aws/lambda/bin/launch.sh
+++ templates/python37/aws/lambda/bin/launch.sh
@@ -4,6 +4,6 @@
 echo "\n ⏱  Launching locally (use ctrl+c to exit)..."
 
 echo "\n 🎯 Example test command:"
-echo " $ curl -XPOST http://localhost:9000/2015-03-31/functions/function/invocations -d '{\"payload\":\"hello world!\"}'\n"
+echo " $ curl -X POST http://localhost:9000/2015-03-31/functions/function/invocations -d '{\"payload\":\"hello world!\"}'\n"
 
 docker run -p 9000:8080 $1
