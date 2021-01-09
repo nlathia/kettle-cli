d4508d71ba25d3774163669bef4e71b7898a3824
Neal Lathia <neal.lathia@gmail.com>
2021-01-09 17:06:01 +0000

    Remove unused template

--- templates/python37/aws/lambda_docker/bin/cleanup.sh
+++
@@ -1,8 +0,0 @@
-# Makefile command: `make clean` 
-# This script removes all of the pycache files from the current directory
-
-echo "\n ⏱  Finding and removing pycache files..."
-
-find . | grep -E "(__pycache__|\.pyc|\.pyo$)" | xargs rm -rf
-
-echo "\n ✅  Done."
