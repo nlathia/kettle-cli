e3e684dea3b664beb9a9477f6a2f3b37425b50b4
Neal Lathia <neal.lathia@gmail.com>
2021-03-10 14:58:25 +0000

    Clean up create command

--- cmd/create.go
+++ cmd/create.go
@@ -22,7 +22,7 @@ import (
 var createCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Create a new project from a template.",
-	Long: `The operator CLI tool can automatically create a directory
+	Long: `The operator CLI tool automatically creates a directory
  with all of the boiler plate that you need from a template.
 	
 The create command will create a directory with all the code to get you started.`,
@@ -35,36 +35,36 @@ func init() {
 }
 
 func validateCreateArgs(cmd *cobra.Command, args []string) error {
-	// Validate that args exist
+	// Validate that a template was given
 	if len(args) == 0 {
-		// @TODO fall back on universal template if it exists
 		return errors.New("please specify a template")
+	}
+	if !strings.HasSuffix(args[0], ".git") {
+		exists, err := templates.PathExists(args[0])
+		if err != nil {
+			return err
+		}
+		if !exists {
+			return fmt.Errorf(fmt.Sprintf("not found: %s", args[0]))
+		}
 	}
 	return nil
 }
 
 func runCreate(cmd *cobra.Command, args []string) error {
-	var err error
-	templatePath := args[0]
-
-	// Inspect templatePath, which can be a local directory or git repo
-	if strings.HasSuffix(templatePath, ".git") {
-		// If it's a git repo: clone it into a tmp directory
-		templatePath, err = ioutil.TempDir("", "operator")
-		if err != nil {
-
-			return err
-		}
+	// Get the directory where the template is (or has been cloned to)
+	templatePath, isTempDir, err := getTemplateDirectory(args[0])
+	if err != nil {
+		return err
+	}
+	if isTempDir {
 		defer os.RemoveAll(templatePath)
+	}
 
-		err = command.Execute("git", []string{
-			"clone",
-			args[0],
-			templatePath,
-		}, "Cloning template...")
-		if err != nil {
-			return err
-		}
+	// Read the template config
+	templateConfig, err := templates.ReadConfig(templatePath)
+	if err != nil {
+		return err
 	}
 
 	// Create the directory where the template will be populated
@@ -73,12 +73,7 @@ func runCreate(cmd *cobra.Command, args []string) error {
 		return cleanUp(directoryPath, err)
 	}
 
-	// Read the template config
-	templateConfig, err := templates.ReadConfig(templatePath)
-	if err != nil {
-		return cleanUp(directoryPath, err)
-	}
-
+	// Ask the user for any input that is required
 	templateValues := map[string]string{
 		"ProjectName": projectName,
 	}
@@ -90,16 +85,7 @@ func runCreate(cmd *cobra.Command, args []string) error {
 		templateValues[templateValue.Key] = userInput
 	}
 
-	// // Set the directory and function name
-	// // Create new config for this deployment, and copy over the global settings
-	// configValues := &config.TemplateConfig{
-	// 	Name:           "testing-templates", //templates.CreateFunctionName(args),
-	// 	FunctionName:   jsonConfig.Config.FunctionName,
-	// 	CloudProvider:  jsonConfig.Config.CloudProvider,
-	// 	DeploymentType: jsonConfig.Config.DeploymentType,
-	// 	Runtime:        jsonConfig.Config.Runtime,
-	// }
-
+	// The template files are in a subdirectory of templatePath
 	templateDirectory := path.Join(templatePath, "template")
 	err = filepath.Walk(templateDirectory, func(filePath string, info fs.FileInfo, err error) error {
 		if err != nil {
@@ -115,41 +101,14 @@ func runCreate(cmd *cobra.Command, args []string) error {
 			return nil
 		}
 
-		// Read the file
-		data, err := ioutil.ReadFile(filePath)
-		if err != nil {
-			return err
-		}
-
 		// Create the target path
 		targetPath := strings.Replace(filePath, templateDirectory, "", 1)
 		targetPath = path.Join(directoryPath, targetPath)
 
-		// Create the parent directory
-		parentDir, _ := path.Split(targetPath)
-		err = os.MkdirAll(parentDir, os.ModePerm)
-		if err != nil {
+		// Create the target file
+		if err := createFile(targetPath, filePath, templateValues); err != nil {
 			return err
 		}
-
-		// Create the file itself
-		f, err := os.Create(targetPath)
-		if err != nil {
-			return err
-		}
-		defer f.Close()
-
-		_, fileName := path.Split(filePath)
-		tmpl, err := template.New(fileName).Parse(string(data))
-		if err != nil {
-			return err
-		}
-
-		err = tmpl.Execute(f, templateValues)
-		if err != nil {
-			return err
-		}
-
 		return nil
 	})
 	if err != nil {
@@ -167,7 +126,7 @@ func runCreate(cmd *cobra.Command, args []string) error {
 }
 
 func createProjectDirectory() (string, string, error) {
-	directoryName, err := command.PromptForString("Project name")
+	directoryName, err := command.PromptForString("Directory name")
 	if err != nil {
 		return "", "", err
 	}
@@ -191,6 +150,63 @@ func createProjectDirectory() (string, string, error) {
 		return "", "", err
 	}
 	return directoryName, directoryPath, nil
+}
+
+func getTemplateDirectory(templatePath string) (string, bool, error) {
+	// templatePath can be a local directory or git repo URL
+	if strings.HasSuffix(templatePath, ".git") {
+		// Git repos will be cloned into a temporary directory
+		tempDirectory, err := ioutil.TempDir("", "operator")
+		if err != nil {
+			return "", false, err
+		}
+
+		err = command.Execute("git", []string{
+			"clone",
+			templatePath,
+			tempDirectory,
+		}, "Cloning template...")
+		if err != nil {
+			return "", false, err
+		}
+		return tempDirectory, true, nil
+	}
+	return templatePath, false, nil
+}
+
+func createFile(targetPath, filePath string, templateValues interface{}) error {
+	// Read the source file
+	data, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
+
+	// Create the parent directory
+	parentDir, _ := path.Split(targetPath)
+	err = os.MkdirAll(parentDir, os.ModePerm)
+	if err != nil {
+		return err
+	}
+
+	// Create the target file
+	f, err := os.Create(targetPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	// Populate the target file by executing the template
+	_, fileName := path.Split(filePath)
+	tmpl, err := template.New(fileName).Parse(string(data))
+	if err != nil {
+		return err
+	}
+
+	err = tmpl.Execute(f, templateValues)
+	if err != nil {
+		return err
+	}
+	return nil
 }
 
 func cleanUp(directoryPath string, err error) error {
--- templates/helpers.go
+++ templates/helpers.go
@@ -1,30 +1,9 @@
 package templates
 
 import (
-	"log"
 	"os"
 	"path"
-	"regexp"
-
-	"github.com/iancoleman/strcase"
 )
-
-func removePunctuation(input, replaceWith string) (string, error) {
-	reg, err := regexp.Compile("[^a-zA-Z0-9]+")
-	if err != nil {
-		return "", err
-	}
-	return reg.ReplaceAllString(input, replaceWith), nil
-}
-
-// The cloud function name is derived from the directory name
-func CreateFunctionName(args []string) string {
-	functionName, err := removePunctuation(args[0], "-")
-	if err != nil {
-		log.Fatal(err)
-	}
-	return strcase.ToKebab(functionName)
-}
 
 // Returns a path that is relative to the current working directory
 func GetRelativeDirectory(directoryName string) (string, error) {
