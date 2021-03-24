bf16809bb6301a532371c4e441a015588ae4f7c5
Neal Lathia <neal.lathia@gmail.com>
2021-03-24 22:26:39 +0000

    Add settings on setup

--- clouds/aws.go
+++ clouds/aws.go
@@ -6,29 +6,31 @@ import (
 	"os/exec"
 
 	"github.com/operatorai/kettle-cli/clouds/aws"
-	"github.com/operatorai/kettle-cli/config"
+	"github.com/operatorai/kettle-cli/settings"
 )
 
 type AmazonWebServices struct{}
 
 func (AmazonWebServices) GetService(deploymentType string) (Service, error) {
 	switch deploymentType {
-	case config.AWSLambda:
+	case "lambda":
 		return aws.AWSLambdaFunction{}, nil
 	}
 	return nil, errors.New(fmt.Sprintf("unimplemented service: %s", deploymentType))
 }
 
-func (AmazonWebServices) Setup(settings *config.Settings) error {
+func (AmazonWebServices) Setup(stg *settings.Settings) error {
 	_, err := exec.LookPath("aws")
 	if err != nil {
 		return errors.New(fmt.Sprintf("please install the aws cli: %s", err))
 	}
-
-	if err := aws.SetAccountID(settings); err != nil {
+	if stg.AWS == nil {
+		stg.AWS = &settings.AWSSettings{}
+	}
+	if err := aws.SetAccountID(stg.AWS); err != nil {
 		return err
 	}
-	if err := aws.SetDeploymentRegion(settings); err != nil {
+	if err := aws.SetDeploymentRegion(stg.AWS); err != nil {
 		return err
 	}
 	return nil
